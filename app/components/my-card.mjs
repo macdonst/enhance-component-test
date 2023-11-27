cae0016e7d65ed65158f6248134b83513f7b0738
Tor Andreas Johnsen <andru_joh@yahoo.no>
2023-11-27 15:23:46 +0100

    Default app with a component

---
+++ app/components/my-card.mjs
@@ -0,0 +1,53 @@
+import enhance from "@enhance/element";
+
+enhance("my-card", {
+  attrs: ["title"],
+  init(element) {
+    console.log("My Card: ", element);
+  },
+  render({ html, state }) {
+    const { attrs = {} } = state;
+    const { title = "default" } = attrs;
+    return html`
+      <style>
+        :host {
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          min-width: 0;
+          word-wrap: break-word;
+          color: black;
+          background-color: #fff;
+          background-clip: border-box;
+          border: 1px solid rgba(0, 0, 0, 0.125);
+          border-radius: 0.25rem;
+        }
+        .card-img {
+          width: 100%;
+          border-top-left-radius: calc(0.25rem - 1px);
+          border-top-right-radius: calc(0.25rem - 1px);
+        }
+        .card-body {
+          flex: 1 1 auto;
+          padding: 1.25rem;
+        }
+        .card-title {
+          margin-bottom: 0.75rem;
+          font-size: 1.25rem;
+          font-weight: 500;
+        }
+      </style>
+      <slot name="image"></slot>
+      <div class="card-body font-sans">
+        <h5 class="card-title">${title}</h5>
+        <slot></slot>
+      </div>
+    `;
+  },
+  connected() {
+    console.log("CONNECTED");
+  },
+  disconnected() {
+    console.log("DISCONNECTED");
+  },
+});
